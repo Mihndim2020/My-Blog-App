2bf2e799b92567648e1273786fa6063643e457b5
Mihndim <jelsieprisca2016@gmail.com>
2021-03-30 12:43:58 -0400

    Add strong parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,16 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
       render :new
     end
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
