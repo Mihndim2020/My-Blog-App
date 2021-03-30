e347930d8f12a1278ec3ec64d49e480cb639d8ee
Mihndim <jelsieprisca2016@gmail.com>
2021-03-30 15:57:30 -0400

    Generate comment controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
   
